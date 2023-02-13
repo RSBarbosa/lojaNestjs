aef44b6e0ce347d5eda73a0a1a2afe9dfcf49133
Renata dos Santos Barbosa <r.dos.santos.barbosa@avanade.com>
2023-02-13 12:33:41 -0300

    CRUD do produto finalizado

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,15 +1,37 @@
-import { Controller, Post, Body, Get } from "@nestjs/common";
-import { CriaProdutoDTO } from './dto/CriarProduto.dto';
+import {  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put, } from "@nestjs/common";
+  import { randomUUID } from 'crypto';
+import { CriaProdutoDTO } from './dto/CriaProduto.dto';
+import { ProdutoEntity } from './produto.entity';
 import { ProdutoRepository } from "./produto.repository";
+import { AtualizaProdutoDTO } from './dto/atualizaProduto.dto';
 
-@Controller( '/produtos')
+@Controller( 'produtos')
 export class ProdutoController {
 
    constructor( private produtoRepository: ProdutoRepository) {}
 
    @Post()
-   criaNovo(@Body() dadosProduto: CriaProdutoDTO) {
-     const produtoCadastrado = this.produtoRepository.salva(dadosProduto);
+   async criaNovo(@Body() dadosProduto: CriaProdutoDTO) {
+    const produto = new ProdutoEntity(); 
+
+    produto.id = randomUUID();
+    produto.nome = dadosProduto.nome;
+    produto.usuarioId = dadosProduto.usuarioId;
+    produto.valor = dadosProduto.valor;
+    produto.quantidade = dadosProduto.quantidade;
+    produto.descricao = dadosProduto.descricao;
+    produto.categoria = dadosProduto.categoria;
+    produto.caracteristicas = dadosProduto.caracteristicas;
+    produto.imagens = dadosProduto.imagens;
+
+
+     const produtoCadastrado = this.produtoRepository.salva(produto);
      return produtoCadastrado;
    }
  
